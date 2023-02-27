2234144bd6218e872c49dc1b3b7d9bab08b44105
[Vadym Fesiuk] <[vadymfesiuk@gmail.com]>
2023-02-27 02:38:47 +0200

    upade imports, added routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from "react";
 import "./js/bootsnav";
-import Home from "./components/Home"
+import Home from "./screens/Home"
 import "./App.css";
 import "./index.css";
 import "../src/css/animate.css";
 import "../src/css/bootsnav.css";
 import "./css/bootstrap.min.css";
 import "../src/css/linearicons.css";
-
-
-
+import SingleProduct from "./screens/SingleProduct";
+import { BrowserRouter as Router, Routes ,Route } from 'react-router-dom';
 
 function App () {
-  return <Home />
+  return (
+    <Router>
+      <Routes>
+      <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<SingleProduct />} />
+      </Routes>
+    </Router>
+  );
 }
-
-
-  
-
 
 export default App;
