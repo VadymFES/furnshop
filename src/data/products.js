07402b6c02c201ac5ff84f7f051dec91fa03833f
[Vadym Fesiuk] <[vadymfesiuk@gmail.com]>
2023-02-27 02:43:44 +0200

    create test DB

---
+++ src/data/products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        _id: "1",
+        name: "wef",
+        image: "https://cdn.dribbble.com/users/4525136/screenshots/17128818/media/e9a54b85f7a6772ffb082cacf98d90db.png",
+        description: "renihghrfojepkpoebknj",
+        price: "234",
+        countInStock: "3",
+        rating: "5",
+        numReviewes: "0",
+    },
+    {
+        _id: "fwe",
+        name: "wefff",
+        image: "",
+        description: "fewaaaaaaa",
+        price: "0",
+        countInStock: "0",
+        rating: "0",
+        numReviewes: "0",
+    },
+    
+];
+
+export default products;
