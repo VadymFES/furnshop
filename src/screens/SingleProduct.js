948a1ea7922b79332c268327c4298c1cd65775c5
[Vadym Fesiuk] <[vadymfesiuk@gmail.com]>
2023-02-28 03:08:52 +0200

    add saving comments and make single product work

--- src/screens/SingleProduct.js
+++ src/screens/SingleProduct.js
@@ -1,12 +1,31 @@
-import React, { useState } from 'react';
-import products  from "../data/products.js";
+import React, { useState } from "react";
+import { useParams } from 'react-router-dom';
+import products from '../data/products';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import StarRating from '../components/StarRating';
-import '../css/SingleProduct.css';
+import Rating from '../components/Rating';
+
+
+
 
 function SingleProduct() {
-  const [product] = useState(products[0]);
+  const { id } = useParams();
+
+
+  const product = products.find((product) => product.id === id);
+  const [comments, setComments] = useState([]);
+
+  
+  if (!product) {
+    return <div>Product not found</div>;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const comment = e.target.comment.value;
+    setComments([...comments, comment]);
+    e.target.reset();
+  };
 
   return (
     <div>
@@ -18,7 +37,7 @@ function SingleProduct() {
         <div className="product-details">
           <h1 className="product-name">{product.name}</h1>
           <div className="product-rating">
-            <StarRating rating={product.rating} />
+            <Rating rating={product.rating} />
             <span className="num-reviews">({product.numReviews} reviews)</span>
           </div>
           <div className="product-price">{product.countInStock > 0 ? `$${product.price}` : "Out of stock"}</div>
@@ -28,10 +47,14 @@ function SingleProduct() {
           <div className="product-comments">
             <h2>Comments</h2>
             <ul>
-              <li>Great product!</li>
-              <li>Love it!</li>
-              <li>Not what I expected.</li>
+              {comments.map((comment, index) => (
+                <li key={index}>{comment}</li>
+              ))}
             </ul>
+            <form onSubmit={handleSubmit}>
+              <input type="text" name="comment" placeholder="Add a comment" />
+              <button type="submit">Submit</button>
+            </form>
           </div>
         </div>
       </div>
