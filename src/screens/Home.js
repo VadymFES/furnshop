160a0615611b7bac9fb598d2d2103bd68f5e56fc
[Vadym Fesiuk] <[vadymfesiuk@gmail.com]>
2023-04-16 19:00:58 +0300

    create addtocart usestate

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,178 +1,186 @@
-import React from "react";
+import React, { useState } from "react";
 import MainBanner from "../components/MainBanner.jsx";
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
 import ShopingSection from "../components/ShopingSection/ShopingSection.jsx";
 
-class Home extends React.Component {
-    render() {
-        return (
-            <div>
+function Home() {
+    const [cartItems, setCartItems] = useState([]);
+    const [favoriteItems, setFavoriteItems] = useState([]);
 
-                <Header />
-                <MainBanner />
-
-
-                <ShopingSection />
-
-
-                <section id="feature" className="feature">
-                    <div className="container">
-                        <div className="section-header">
-                            <h2>featured products</h2>
-                        </div>
-                        <div className="feature-content">
-                            <div className="row">
-                                <div className="col-sm-3">
-                                    <div className="single-feature">
-                                        <img src="assets/images/features/f1.jpg" alt="feature" />
-                                        <div className="single-feature-txt text-center">
-                                            <p>
-                                                <i className="fa fa-star"></i>
-                                                <i className="fa fa-star"></i>
-                                                <i className="fa fa-star"></i>
-                                                <i className="fa fa-star"></i>
-                                                <span className="spacial-feature-icon"><i className="fa fa-star"></i></span>
-                                                <span className="feature-review">(45 review)</span>
-                                            </p>
-                                            <h3><a href="//#">designed sofa</a></h3>
-                                            <h5>$160.00</h5>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-sm-3">
-                                    <div className="single-feature">
-                                        <img src="assets/images/features/f2.jpg" alt="feature" />
-                                        <div className="single-feature-txt text-center">
-                                            <p>
-                                                <i className="fa fa-star"></i>
-                                                <i className="fa fa-star"></i>
-                                                <i className="fa fa-star"></i>
-                                                <i className="fa fa-star"></i>
-                                                <span className="spacial-feature-icon"><i className="fa fa-star"></i></span>
-                                                <span className="feature-review">(45 review)</span>
-                                            </p>
-                                            <h3><a href="//#">dinning table </a></h3>
-                                            <h5>$200.00</h5>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-sm-3">
-                                    <div className="single-feature">
-                                        <img src="assets/images/features/f3.jpg" alt="feature" />
-                                        <div className="single-feature-txt text-center">
-                                            <p>
-                                                <i className="fa fa-star"></i>
-                                                <i className="fa fa-star"></i>
-                                                <i className="fa fa-star"></i>
-                                                <i className="fa fa-star"></i>
-                                                <span className="spacial-feature-icon"><i className="fa fa-star"></i></span>
-                                                <span className="feature-review">(45 review)</span>
-                                            </p>
-                                            <h3><a href="//#">chair and table</a></h3>
-                                            <h5>$100.00</h5>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-sm-3">
-                                    <div className="single-feature">
-                                        <img src="assets/images/features/f4.jpg" alt="feature" />
-                                        <div className="single-feature-txt text-center">
-                                            <p>
-                                                <i className="fa fa-star"></i>
-                                                <i className="fa fa-star"></i>
-                                                <i className="fa fa-star"></i>
-                                                <i className="fa fa-star"></i>
-                                                <span className="spacial-feature-icon"><i className="fa fa-star"></i></span>
-                                                <span className="feature-review">(45 review)</span>
-                                            </p>
-                                            <h3><a href="//#">modern arm chair</a></h3>
-                                            <h5>$299.00</h5>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </section>
-
-
-
-                <section id="blog" className="blog">
-                    <div className="container">
-                        <div className="section-header">
-                            <h2>latest blog</h2>
-                        </div>
-                        <div className="blog-content">
-                            <div className="row">
-                                <div className="col-sm-4">
-                                    <div className="single-blog">
-                                        <div className="single-blog-img">
-                                            <img src="assets/images/blog/b1.jpg" alt="blog" />
-                                            <div className="single-blog-img-overlay"></div>
-                                        </div>
-                                        <div className="single-blog-txt">
-                                            <h2><a href="//#">Why Brands are Looking at Local Language</a></h2>
-                                            <h3>By <a href="//#">Robert Norby</a> / 18th March 2018</h3>
-                                            <p>
-                                                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt....
-                                            </p>
-                                        </div>
-                                    </div>
-
-                                </div>
-                                <div className="col-sm-4">
-                                    <div className="single-blog">
-                                        <div className="single-blog-img">
-                                            <img src="assets/images/blog/b2.jpg" alt="blog" />
-                                            <div className="single-blog-img-overlay"></div>
-                                        </div>
-                                        <div className="single-blog-txt">
-                                            <h2><a href="//#">Why Brands are Looking at Local Language</a></h2>
-                                            <h3>By <a href="//#">Robert Norby</a> / 18th March 2018</h3>
-                                            <p>
-                                                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt....
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-sm-4">
-                                    <div className="single-blog">
-                                        <div className="single-blog-img">
-                                            <img src="assets/images/blog/b3.jpg" alt="blog " />
-                                            <div className="single-blog-img-overlay"></div>
-                                        </div>
-                                        <div className="single-blog-txt">
-                                            <h2><a href="//#">Why Brands are Looking at Local Language</a></h2>
-                                            <h3>By <a href="//#">Robert Norby</a> / 18th March 2018</h3>
-                                            <p>
-                                                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt....
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </section>
-
-
-
-                <Footer />
-
-                
-
-            </div>
-
-
-
-
-
-
-        )
+    const addToFavorite = (product) => {
+        setFavoriteItems([...favoriteItems, product]);
     };
-}
+    const addToCart = (product) => {
+        setCartItems([...cartItems, product]);
+    };
+
+    return (
+        <div>
+            <Header cartItems={cartItems} favoriteItems={favoriteItems} />
+            <MainBanner />
+
+
+            <ShopingSection addToCart={addToCart} addToFavorite={addToFavorite} />
+
+
+            <section id="feature" className="feature">
+                <div className="container">
+                    <div className="section-header">
+                        <h2>featured products</h2>
+                    </div>
+                    <div className="feature-content">
+                        <div className="row">
+                            <div className="col-sm-3">
+                                <div className="single-feature">
+                                    <img src="assets/images/features/f1.jpg" alt="feature" />
+                                    <div className="single-feature-txt text-center">
+                                        <p>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <span className="spacial-feature-icon"><i className="fa fa-star"></i></span>
+                                            <span className="feature-review">(45 review)</span>
+                                        </p>
+                                        <h3><a href="//#">designed sofa</a></h3>
+                                        <h5>$160.00</h5>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm-3">
+                                <div className="single-feature">
+                                    <img src="assets/images/features/f2.jpg" alt="feature" />
+                                    <div className="single-feature-txt text-center">
+                                        <p>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <span className="spacial-feature-icon"><i className="fa fa-star"></i></span>
+                                            <span className="feature-review">(45 review)</span>
+                                        </p>
+                                        <h3><a href="//#">dinning table </a></h3>
+                                        <h5>$200.00</h5>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm-3">
+                                <div className="single-feature">
+                                    <img src="assets/images/features/f3.jpg" alt="feature" />
+                                    <div className="single-feature-txt text-center">
+                                        <p>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <span className="spacial-feature-icon"><i className="fa fa-star"></i></span>
+                                            <span className="feature-review">(45 review)</span>
+                                        </p>
+                                        <h3><a href="//#">chair and table</a></h3>
+                                        <h5>$100.00</h5>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm-3">
+                                <div className="single-feature">
+                                    <img src="assets/images/features/f4.jpg" alt="feature" />
+                                    <div className="single-feature-txt text-center">
+                                        <p>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <span className="spacial-feature-icon"><i className="fa fa-star"></i></span>
+                                            <span className="feature-review">(45 review)</span>
+                                        </p>
+                                        <h3><a href="//#">modern arm chair</a></h3>
+                                        <h5>$299.00</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+
+
+
+            <section id="blog" className="blog">
+                <div className="container">
+                    <div className="section-header">
+                        <h2>latest blog</h2>
+                    </div>
+                    <div className="blog-content">
+                        <div className="row">
+                            <div className="col-sm-4">
+                                <div className="single-blog">
+                                    <div className="single-blog-img">
+                                        <img src="assets/images/blog/b1.jpg" alt="blog" />
+                                        <div className="single-blog-img-overlay"></div>
+                                    </div>
+                                    <div className="single-blog-txt">
+                                        <h2><a href="//#">Why Brands are Looking at Local Language</a></h2>
+                                        <h3>By <a href="//#">Robert Norby</a> / 18th March 2018</h3>
+                                        <p>
+                                            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt....
+                                        </p>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col-sm-4">
+                                <div className="single-blog">
+                                    <div className="single-blog-img">
+                                        <img src="assets/images/blog/b2.jpg" alt="blog" />
+                                        <div className="single-blog-img-overlay"></div>
+                                    </div>
+                                    <div className="single-blog-txt">
+                                        <h2><a href="//#">Why Brands are Looking at Local Language</a></h2>
+                                        <h3>By <a href="//#">Robert Norby</a> / 18th March 2018</h3>
+                                        <p>
+                                            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt....
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm-4">
+                                <div className="single-blog">
+                                    <div className="single-blog-img">
+                                        <img src="assets/images/blog/b3.jpg" alt="blog " />
+                                        <div className="single-blog-img-overlay"></div>
+                                    </div>
+                                    <div className="single-blog-txt">
+                                        <h2><a href="//#">Why Brands are Looking at Local Language</a></h2>
+                                        <h3>By <a href="//#">Robert Norby</a> / 18th March 2018</h3>
+                                        <p>
+                                            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt....
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+
+
+
+            <Footer />
+
+
+
+        </div>
+
+
+
+
+
+
+    )
+};
+
 
 export default Home;
