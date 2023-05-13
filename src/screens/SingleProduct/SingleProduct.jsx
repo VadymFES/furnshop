c0305c3f6d32e2ffe92bb8fed7afe3e15efe3d59
[Vadym Fesiuk] <[vadymfesiuk@gmail.com]>
2023-05-13 22:54:42 +0300

    move public filder back

--- src/screens/SingleProduct/SingleProduct.jsx
+++ src/screens/SingleProduct/SingleProduct.jsx
@@ -63,7 +63,7 @@ function SingleProduct() {
     <div key={product.id}>
       <Header />
       <div className="product-page">
-        <nav style={{ '--bs-breadcrumb-divider': 'url(\'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'8\' height=\'8\'%3E%3Cpath d=\'M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z\' fill=\'currentColor\'/%3E%3C/svg%3E\')' }} aria-label="breadcrumb">
+        <nav style={{  }} aria-label="breadcrumb">
           <div className="breadcrumb-item">
             <ol className="breadcrumb">
               <li className="breadcrumb-item"><Link to="/">Головна</Link> </li>
